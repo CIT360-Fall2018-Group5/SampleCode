c8279406797e401e1ad2f7e3c879ce9b0979d014
Jenson-Isabel <35979317+Jenson-Isabel@users.noreply.github.com>
2018-12-09 15:45:54 -0700

    Threads, Runnables, Executables

---
+++ SimpleThreadPool.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Isabel Jenson (https://www.journaldev.com/1069/threadpoolexecutor-java-thread-pool-example-executorservice)
+ */
+
+package Threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleThreadPool {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            Runnable worker = new WorkerThread("" + i);
+            executor.execute(worker);
+          }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        System.out.println("Finished all threads");
+    }
+}
---
+++ WorkerThread.java
@@ -0,0 +1,45 @@
+package Threads;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Isabel Jenson(https://www.journaldev.com/1069/threadpoolexecutor-java-thread-pool-example-executorservice)
+ */
+
+
+
+
+public class WorkerThread implements Runnable {
+  
+    private String command;
+    
+    public WorkerThread(String s){
+        this.command=s;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+" Start. Command = "+command);
+        processCommand();
+        System.out.println(Thread.currentThread().getName()+" End.");
+    }
+
+    private void processCommand() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return this.command;
+    }
+}
